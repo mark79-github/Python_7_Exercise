caad579d69aef1096d4a6ffff2e8152d7d8d235b
mark79-github <52877024+mark79-github@users.noreply.github.com>
2026-04-04 06:04:49 +0300

    Add shevitsi_7a_4_2.py for drawing a sequence of filled red triangles

---
+++ shevitsi_7a_4_2.py
@@ -0,0 +1,38 @@
+import turtle
+screen = turtle.Screen()
+p = turtle.Turtle()
+p.up()
+
+def triangle(side):
+  p.begin_fill()
+  p.down()
+  for count in range(3):
+    p.forward(side)
+    p.left(120)
+  p.end_fill()
+  p.up()
+
+pos=100
+color='red'
+side=30
+p.goto(pos,0)
+p.right(30)
+p.color(color)
+
+for br in range(3):
+  triangle(side)
+  pos = pos - side*0.9
+  p.goto(pos,0)
+
+pos = pos - side * 0.9
+p.goto(pos,0)
+triangle(side*2)
+p.right(180)
+triangle(side*2)
+pos = pos - 2 * 0.9 * side
+p.goto(pos,0)
+
+for br in range(3):
+  triangle(side)
+  pos = pos - 0.9*side
+  p.goto(pos,0)
